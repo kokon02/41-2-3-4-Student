89a24b64296f9ef603d665aa47c491ee0d0a9102
kokon02 <127319252+kokon02@users.noreply.github.com>
2023-05-17 13:30:29 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Sudent{
+  private String Name;
+  private int Age;
+  private String Secondname;
+  public Student(String name,String secondname, int age){
+    Name = name;
+    Age = age;
+    Secondname = secondname;
+  }
+  public String GetName() {return Name;}
+  public String GetSecondname() {return Secondname;}
+  public int GetAge() {return Age;}
+
+  pubic String ToString(){
+    return Name + " " + Secondname + " " + Integer.toString(Age);
+  }
+  public static Student Parse(String str){
+    String[] data = str.split(" ");
+    if(data.lenght !=3)
+      return new Student("Parse Error", "sss", -1);
+    return new Student(data[0],data[1], Integer.parseInt(data[2]));
+  }
+}
